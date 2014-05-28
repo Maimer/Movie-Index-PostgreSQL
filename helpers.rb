f9d3968930b4764bbd66411a8e43de57b1e06e96
Maimer <nesnl23@yahoo.com>
2014-05-28 15:08:27 -0400

    Added movie and actor pages, and added pagination

---
+++ helpers.rb
@@ -0,0 +1,82 @@
+require 'pg'
+
+def db_connection
+  begin
+    connection = PG.connect(dbname: 'movies')
+
+    yield(connection)
+
+  ensure
+    connection.close
+  end
+end
+
+def get_movies(order, page)
+  db_connection do |conn|
+    query = "SELECT movies.title, movies.year, movies.rating, movies.id, genres.name AS genre, studios.name
+             AS studio
+             FROM movies
+               JOIN genres ON movies.genre_id = genres.id
+               JOIN studios ON movies.studio_id = studios.id
+             ORDER BY movies.#{order}
+             LIMIT 20
+             OFFSET #{(page * 20) - 20}"
+    conn.exec(query)
+  end
+end
+
+def get_movie_detail(id)
+  db_connection do |conn|
+    query = "SELECT movies.title, movies.year, genres.name AS genre, studios.name AS studio, actors.id, actors.name
+             AS actor, cast_members.character
+             FROM movies
+               INNER JOIN genres ON movies.genre_id = genres.id
+               INNER JOIN studios ON movies.studio_id = studios.id
+               JOIN cast_members ON movies.id = cast_members.movie_id
+               JOIN actors ON cast_members.actor_id = actors.id
+             WHERE movies.id = #{id}"
+    conn.exec(query)
+  end
+end
+
+def get_actors(page)
+  db_connection do |conn|
+    query = "SELECT actors.name, actors.id FROM actors
+             ORDER BY actors.name
+             LIMIT 20
+             OFFSET #{(page * 20) - 20}"
+    conn.exec(query)
+  end
+end
+
+def get_actor_detail(id)
+  db_connection do |conn|
+    query = "SELECT actors.name, movies.title, movies.id, cast_members.character
+               FROM actors
+               JOIN cast_members ON actors.id = cast_members.actor_id
+               JOIN movies ON movies.id = cast_members.movie_id
+             WHERE actors.id = #{id}"
+    conn.exec(query)
+  end
+end
+
+def search(type, terms)
+  if type == "movies"
+    query = "SELECT movies.title, movies.year, movies.rating, movies.id, movies.synopsis, genres.name AS genre, studios.name
+             AS studio
+             FROM movies
+               JOIN genres ON movies.genre_id = genres.id
+               JOIN studios ON movies.studio_id = studios.id
+             ORDER BY movies.#{order}
+             WHERE movies.title, movies.synopsis LIKE '%$1%'"
+  elsif type == "actors"
+    query = "SELECT actors.name, actors.id FROM actors
+             ORDER BY actors.name
+             WHERE actors.name LIKE '%$1%'"
+  end
+
+  db_connection do |conn|
+
+    conn.exec_params(query, [terms])
+  end
+end
---
+++ server.rb
@@ -0,0 +1,53 @@
+require 'sinatra'
+require_relative 'helpers.rb'
+
+get '/' do
+  redirect '/movies'
+end
+
+get '/movies' do
+  @title = "MOVIES INDEX"
+  if params[:order] != nil
+    @order = params[:order]
+  else
+    @order = "title"
+  end
+  if params[:page] != nil
+    @page = params[:page].to_i
+  else
+    @page = 1
+  end
+  if params[:query] == nil
+    @movies = get_movies(@order, @page).to_a
+  else
+    @movies = search("movies", params[:query])
+  end
+  erb :'movies/index'
+end
+
+get '/movies/:id' do
+  @movie = get_movie_detail(params[:id])
+  @title = @movie[0]["title"]
+  erb :'movies/show'
+end
+
+get '/actors' do
+  @title = "ACTORS INDEX"
+  if params[:page] != nil
+    @page = params[:page].to_i
+  else
+    @page = 1
+  end
+  if params[:query] == nil
+    @actors = get_actors(@page).to_a
+  else
+    @movies = search("actors", params[:query])
+  end
+  erb :'actors/index'
+end
+
+get '/actors/:id' do
+  @actor = get_actor_detail(params[:id])
+  @title = @actor[0]["name"]
+  erb :'actors/show'
+end
